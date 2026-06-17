480eb1217644f1428038c8e2c92a8acaf380243d
Sumeet Verma <sumeet.verma@acldigital.com>
2026-06-17 20:04:28 +0530

    Added code for below: "1. Install Python & check version 2. Print name, age, boolean 3. Create username & password variables" Store product list using Python list Build login validation (valid vs invalid credentials) Loop through product list and print each item

---
+++ Firstprogram.py
@@ -0,0 +1,49 @@
+#print ("Sumeet Verma")
+#Age : 36
+#print ("I am 36 years old")
+
+# Create username & password variables
+
+# username = "Sumeet Verma"
+# password = "Sumeet@123"
+
+# Store product list using Python list
+
+#x = ["apple", "banana", "cherry"]
+
+# Build login validation (valid vs invalid credentials)
+
+# input_username = input("Enter your username: ")
+# input_password = input("Enter your password: ")
+
+# def validate_login(input_username, input_password):
+#     if input_username == username and input_password == password:
+#         print("valid credentials")
+#         return True
+        
+#     else:
+#         print("invalid credentials")
+#         return False
+# store=validate_login(input_username, input_password) 
+# print(store)
+
+
+# a = 20
+# b = 20
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+# Loop through product list and print each item
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+  if fruit == "apple":
+     print(fruit)
+  elif fruit == "banana":
+     print(fruit)
+  elif fruit == "cherry":
+     print(fruit)
